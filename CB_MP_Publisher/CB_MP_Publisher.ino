934643349b0b56e121da2987b186d3747d01ef66
miikey2 <32358722+miikey2@users.noreply.github.com>
2018-01-04 15:08:00 +0000

    done
    done

--- CB_MP_Publisher/CB_MP_Publisher.ino
+++ CB_MP_Publisher/CB_MP_Publisher.ino
@@ -1,3 +1,4 @@
+#include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
 #include <PubSubClient.h>
 #include <SPI.h>
@@ -7,7 +8,7 @@
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to.
-const int rs = 10, en = 9 , d4 = 5, d5 = 4, d6 = 3, d7 = 7;
+const int rs = 7, en = 8 , d4 = 5, d5 = 4, d6 = 3, d7 = 6;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Needs changing for any different boards code is uploaded to.
@@ -23,25 +24,23 @@ EthernetClient ethernetClient;
 PubSubClient client(ethernetClient);
 
 // Pin Constants defined here
-const char pirsensor = 2;
+const int pirsensor = A4;
 const int lightsensor = A2;
 const int tempsensor = A3;
-const int humidsensor = A4;
-const int moisturesensor = A5;
+const int humidsensor = A0;
+const int moisturesensor = A1;
 
 // Programme scope variables declared here
 uint8_t pirState = 0;
 unsigned int maxLevel = 0;
 unsigned int minLevel = 1023;
 int light;
-int PIR;
-int temperature;
+int pirVal;
 int humidity;
 int moisture;
-float tempval;
-int count = 1;
-int count2 = 1;
-unsigned long ts2 = millis();
+float tempVal;
+unsigned long ts3  = millis();
+uint8_t count3 = 1;
 
 
   void setup() {
@@ -59,6 +58,7 @@ unsigned long ts2 = millis();
   /*!SET PINS HERE!*/ 
   // Set input and outputs
   pinMode(pirsensor, INPUT);
+  digitalWrite(pirsensor, LOW);
   pinMode(lightsensor, INPUT);
   pinMode(tempsensor, INPUT);
   pinMode(humidsensor, INPUT);
@@ -87,73 +87,84 @@ void loop() {
     reconnect();
   }
   
-  unsigned long ts = millis();
+  unsigned long ts2 = millis();
+  uint8_t count2 = 1;
   // Every 0.2 seconds, read different sensor values to stagger publishing
-  while (millis() < (ts + 1000)){
-  if ((count == 1) && (millis() < ts + 200)){
-    count++;
-    readPIR(&pirState);
+  while (millis() < (ts2 + 1000)){
+  if ((count2 == 1) && (millis() < ts2 + 200)){
+    count2++;
+    read_PIR(&pirState);
   }
-  else if ((count == 2) && (millis() > ts + 200) && (millis() < ts + 400)){
-    count++;
-    sendLight(&maxLevel, &minLevel);
+  else if ((count2 == 2) && (millis() > ts2 + 200) && (millis() < ts2 + 400)){
+    count2++;
+    send_Light(&maxLevel, &minLevel);
   }
-  else if ((count == 3) && (millis() > ts + 400) && (millis() < ts + 600)){
-    count++;
-    sendTemp();
+  else if ((count2 == 3) && (millis() > ts2 + 400) && (millis() < ts2 + 600)){
+    count2++;
+    send_Temp();
   }
-  else if ((count == 4) && (millis() > ts + 600) && (millis() < ts + 800)){
-    count++;
-    sendHumidity();
+  else if ((count2 == 4) && (millis() > ts2 + 600) && (millis() < ts2 + 800)){
+    count2++;
+    send_Humidity();
   }
-  else if ((count == 5) && (millis() > ts + 800) && (millis() < ts + 1000)){
-    sendMoisture();
-    count = 1;
+  else if ((count2 == 5) && (millis() > ts2 + 800) && (millis() < ts2 + 1000)){
+    send_Moisture();
+    count2 = 1;
   }
  }
  
 
+
 // Every 5 seconds, print new sensor value to LCD
-while (millis() > (ts2 + 5000)){
-  if (count2 == 1){
+if (millis() > (ts3 + 2000)){
+
+  switch(count3){
+    
+  case 1:
     lcd.clear();
     lcd.print("PIR state = ");
-    lcd.print(PIR, 1);
-    count2++;
-    ts2 = millis();
-  }
-  else if (count2 == 2){
+    lcd.print(pirVal, 1);
+    count3++;
+    ts3 = millis();
+    break;
+    
+  case 2:
     lcd.clear();
     lcd.print("Light = ");
     lcd.print(light, 1);
     lcd.print("%");
-    count2++;
-    ts2 = millis();
-  }
-  else if (count2 == 3){
+    count3++;
+    ts3 = millis();
+    break;
+    
+  case 3:
     lcd.clear();
     lcd.print("Temp = ");
-    lcd.print(tempval, 1);
+    lcd.print(tempVal, 1);
     lcd.print((char)223);
     lcd.print("C");
-    count2++;
-    ts2 = millis();
-  }
-  else if (count2 == 4){
+    count3++;
+    ts3 = millis();
+    break;
+    
+  case 4:
     lcd.clear();
     lcd.print("Humidity = ");
     lcd.print(humidity, 1);
     lcd.print("%");
-    count2++;
-    ts2 = millis();
-  }
-  else if (count2 == 5){
+    count3++;
+    ts3 = millis();
+    break;
+    
+  case 5:
+  
     lcd.clear();
     lcd.print("Moisture = ");
     lcd.print(moisture, 1);
     lcd.print("%");
-    count2 = 1;
-    ts2 = millis();
+    count3 = 1;
+    ts3 = millis();
+    break;
   }
 }
 
@@ -190,7 +201,7 @@ void reconnect() {
 
     // Connect Publisher to broker
     /*!CHANGE BROKER AND FEEDS TO brain.engineering and /f/... !*/
-    if (client.connect("CB_MP_Publisher", "cbraines", "Tp:5tF'<5dc_k@;<")) {
+    if (client.connect("CB_MP_Publisher", "mpearson", "passw0rd")) {
       Serial.println("... connected");
 
       client.publish("cbraines/f/message-log", "Publisher begun");
@@ -198,12 +209,12 @@ void reconnect() {
       /*!ADD FEEDS!*/
       /*!NEED TO KNOW STATES OF ANY CALIBRATION FEEDS!*/
       // Subscribe to relevant feeds
-      client.subscribe("/mpearson/f/light-level"); /*!DON'T NEED FURTHER DOWN THE LINES!*/
-      client.subscribe("/mpearson/f/temp-level"); 
-      client.subscribe("/mpearson/f/humidity-level"); 
-      client.subscribe("/mpearson/f/moisture-level"); 
-      client.subscribe("/mpearson/f/pir-level"); 
-      client.subscribe("/mpearson/f/recalibrate-ldr");
+     // client.subscribe("/f/light-level"); /*!DON'T NEED FURTHER DOWN THE LINES!*/
+     // client.subscribe("/f/temp-level"); 
+     // client.subscribe("/f/humidity-level"); 
+     // client.subscribe("/f/moisture-level"); 
+     // client.subscribe("/f/pir-level"); 
+     // client.subscribe("/f/recalibrate-ldr");
 
       // Perform setup tasks
       calibrate_Ldr(&maxLevel, &minLevel);
@@ -226,30 +237,30 @@ void reconnect() {
 // Re-calibration function
 void calibrate_Ldr(uint16_t *maxL, uint16_t *minL){
   
-  unsigned long timestamp = millis();
-  uint8_t i = 1;
+  unsigned long ts = millis();
+  uint8_t count = 1;
   *maxL = 0;
   *minL = 1023;
   Serial.println("Begin calibration now:");
-  while (millis() < (timestamp + 5000)){
-    if ((i == 1) && (millis() < timestamp + 1000)){
-      i++;
+  while (millis() < (ts + 5000)){
+    if ((count == 1) && (millis() < ts + 1000)){
+      count++;
       Serial.println("5");
     }
-    else if ((i == 2) && (millis() > timestamp + 1000) && (millis() < timestamp + 2000)){
-      i++;
+    else if ((count == 2) && (millis() > ts + 1000) && (millis() < ts + 2000)){
+      count++;
       Serial.println("4");
     }
-    else if ((i == 3) && (millis() > timestamp + 2000) && (millis() < timestamp + 3000)){
-      i++;
+    else if ((count == 3) && (millis() > ts + 2000) && (millis() < ts + 3000)){
+      count++;
       Serial.println("3");
     }
-    else if ((i == 4) && (millis() > timestamp + 3000) && (millis() < timestamp + 4000)){
-      i++;
+    else if ((count == 4) && (millis() > ts + 3000) && (millis() < ts + 4000)){
+      count++;
       Serial.println("2");
     }
-    else if ((i == 5) && (millis() > timestamp + 4000) && (millis() < timestamp + 5000)){
-      i++;
+    else if ((count == 5) && (millis() > ts + 4000) && (millis() < ts + 5000)){
+      count++;
       Serial.println("1");
     }
     int light = analogRead(A0);
@@ -260,12 +271,12 @@ void calibrate_Ldr(uint16_t *maxL, uint16_t *minL){
       *minL = light;
     }
   }
-  client.publish("mpearson/f/message-log","LDR recalibrated");
+  client.publish("/f/message-log","LDR recalibrated");
 }
 
 
 // Function to read light and send via sendFeed function
-void sendLight(uint16_t *maxL, uint16_t *minL){
+void send_Light(uint16_t *maxL, uint16_t *minL){
   if (client.connected()){
     light = analogRead(lightsensor);
 
@@ -274,62 +285,62 @@ void sendLight(uint16_t *maxL, uint16_t *minL){
     // Stop value from exceeding upper and lower bounds
     light = constrain(light, 0, 100);
     
-    char feed[] = "/mpearson/f/light-level";
-    sendData(feed, light);
+    char feed[] = "/f/light-level";
+    send_Data(feed, light);
   }
 }
 
 // Function for PIR that Transmits PIR state on toggle of state
-void readPIR(uint8_t *state){
+void read_PIR(uint8_t *state){
   /* PIR holds input of 1 internally for ~8s whenever detecting so no interrupt needed */ 
-  PIR = digitalRead(pirsensor);
-      
+  pirVal = digitalRead(pirsensor);
+
   // tx broker if latch has occured
-  if (PIR != *state){
-      char feed[] = "/mpearson/f/pir-level";
-      sendData(feed, PIR);
-      *state = PIR;
+  if (pirVal != *state){
+      char feed[] = "/f/pir-level";
+      send_Data(feed, pirVal);
+      *state = pirVal;
   }
 }
 
 // Function to read temp and send via sendFeed function
-void sendTemp(){
+void send_Temp(){
   if (client.connected()){
     
-    temperature = analogRead(tempsensor);
+    int temperature = analogRead(tempsensor);
 
     float voltage = ((temperature * 5.0) / 1023.0);
-    tempval = ((voltage - 0.5) * 100.0);
+    tempVal = ((voltage - 0.5) * 100.0);
 
-    char feed[] = "/mpearson/f/temp-level";
-    sendData(feed, tempval);
+    char feed[] = "/f/temp-level";
+    send_Data(feed, tempVal);
   }
 }
 
 // Function to read humidity and send via sendFeed function
-void sendHumidity(){
+void send_Humidity(){
   if (client.connected()){
     humidity = analogRead(humidsensor);
 
-    humidity = constrain(humidity, 0, 100);
-    char feed[] = "/mpearson/f/humidity-level";
-    sendData(feed, humidity);
+    humidity = map(humidity, 0, 1023, 0, 100);
+    char feed[] = "/f/humidity-level";
+    send_Data(feed, humidity);
   }
 }
 
 // Function to read moisture and send via sendFeed function
-void sendMoisture(){
+void send_Moisture(){
   if (client.connected()){
     moisture = analogRead(moisturesensor);
 
-    moisture = constrain(moisture, 0, 100);
-    char feed[] = "/mpearson/f/moisture-level";
-    sendData(feed, moisture);
+    moisture = map(moisture, 0, 1023, 0, 100);
+    char feed[] = "/f/moisture-level";
+    send_Data(feed, moisture);
   }
 }
 
 // Function to handle all data sending
-void sendData(char *feed, int payload){
+void send_Data(char *feed, int payload){
 
   // Convert payload to character array
   String payloadStr = String(payload, DEC);
